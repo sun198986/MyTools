47f0173960273934c52d5c8b69d33934bc03f2f6
sun198986 <334828328@qq.com>
2020-05-28 15:29:14 +0800

    add office component class

---
+++ Word.OfficeCOM/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Word.OfficeCOM.Utils;
+
+namespace Word.OfficeCOM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currDir = AppDomain.CurrentDomain.BaseDirectory;
+            string fileFullName = string.Format(@"{0}demo\标准模板(新).docx", currDir);
+            string outFileFullName = string.Format(@"{0}demo\{1}.docx", currDir,Guid.NewGuid().ToString());
+            string pictureFullName = string.Format(@"{0}demo\", currDir);
+            using (WordUtil wordUtil = new WordUtil(outFileFullName, fileFullName)) { 
+                
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
