5c1a67e06b1d77acebc4318ee2b811d5b9e6b1de
sun198986 <334828328@qq.com>
2020-05-31 03:38:10 +0800

    attach compress string project

---
+++ Framework.ZipString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.ZipString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+            string sourceString = "123";
+            string result =  ZipStringHelper.CompressString(sourceString);
+            Console.WriteLine(result);
+            Console.ReadLine();
+            string decompress = ZipStringHelper.DecompressString(result);
+            Console.WriteLine(decompress);
+            Console.ReadLine();
+        }
+    }
+}
