307a127e259f256c233390ab4dedcaee7eb354b6
sun198986 <334828328@qq.com>
2020-07-04 02:17:27 +0800

    wordOpenxml demo

---
+++ WordOpenXml/Program.cs
@@ -0,0 +1,17 @@
+﻿/***
+ * https://docs.microsoft.com/zh-cn/dotnet/api/documentformat.openxml.wordprocessing?view=openxml-2.8.1
+ * */
+using System;
+
+namespace WordOpenXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = $"{AppDomain.CurrentDomain.BaseDirectory}\\Demo\\demo.docx";
+            WordXml.CreateWordDoc(filepath, "你好");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
