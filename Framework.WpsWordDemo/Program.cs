a0a22e28827fad741a1b3a461e76b094dd89f435
sun198986 <334828328@qq.com>
2020-07-06 14:46:59 +0800

    wpsword测试

---
+++ Framework.WpsWordDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using Framework.FileUtil.Interface;
+using Framework.ProcessUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.WpsWordDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessControl processControl = new ConcreteProcessControl();
+            processControl.KillWordProcess();
+            string currDir = AppDomain.CurrentDomain.BaseDirectory;
+            string fileFullName = string.Format(@"{0}Demo\pdf导出.docx", currDir);
+            string outDirctoryUrl = string.Format(@"{0}NewFiles", currDir);
+            FileControl fileControl = new ConcreteFileControl();
+            fileControl.CreateDirectory(outDirctoryUrl);
+            string outFileFullName = string.Format(@"{0}NewFiles\{1}.docx", currDir, Guid.NewGuid().ToString());
+            using (ToPdfHelper tph = new ToPdfHelper("docx")) {
+                tph.WordWpsToPdf(fileFullName);
+            }
+        }
+    }
+}
---
+++ Framework.WpsWordDemo/WpsTest.cs
@@ -0,0 +1,124 @@
+﻿using PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word;
+
+namespace Framework.WpsWordDemo
+{
+    public class ToPdfHelper : IDisposable
+
+    {
+
+        dynamic wps;
+
+        public ToPdfHelper(string typeName)
+
+        {
+
+            if (typeName == "xls")
+
+                typeName = "KET.Application";
+
+            else if (typeName == "ppt")
+
+                typeName = "KWPP.Application";
+
+            else
+
+                typeName = "KWps.Application";
+
+            //创建wps实例，需提前安装wps
+
+            Type type = Type.GetTypeFromProgID(typeName);
+
+            if (type == null)
+
+                type = Type.GetTypeFromProgID("wps.Application");
+
+            wps = Activator.CreateInstance(type);
+
+        }
+
+        /// <summary>
+
+        /// 使用wps将Word转PDF
+
+        /// </summary>
+
+        /// <param name="saveUrl">文件路径</param>
+
+        /// <param name="targetPath">源文件路径</param>
+
+        /// <returns></returns>
+
+        public string WordWpsToPdf( string targetPath)
+
+        {
+
+            if (targetPath == null)
+
+            {
+
+                throw new ArgumentNullException("wpsFilename");
+
+            }
+
+            var wordPath =  targetPath;
+
+            var pdfPath = Path.ChangeExtension(wordPath, "pdf");
+
+            try
+
+            {
+
+                //用wps 打开word不显示界面
+
+                dynamic doc = wps.Documents.Open(wordPath, Visible: false);
+
+                //doc 转pdf
+
+                doc.ExportAsFixedFormat(pdfPath, WdExportFormat.wdExportFormatPDF);
+
+                //设置隐藏菜单栏和工具栏
+
+                //wps.setViewerPreferences(PdfWriter.HideMenubar | PdfWriter.HideToolbar);
+
+                doc.Close();
+
+                doc = null;
+
+            }
+
+            catch (Exception)
+
+            {
+
+                //targetPath = GetEXCELtoPDF.CreatePDFs(saveUrl, targetPath);
+
+            }
+
+            finally
+
+            {
+
+                Dispose();
+
+            }
+
+            return Path.ChangeExtension(targetPath, "pdf");
+
+        }
+
+
+        public void Dispose()
+
+        {
+            if (wps != null) { wps.Quit(); wps = null; }
+        }
+
+    }
+}
