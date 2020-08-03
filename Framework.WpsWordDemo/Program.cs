97629487a4b0faa6eeacbc16f3f858971ff12390
sun198986 <334828328@qq.com>
2020-08-03 16:19:44 +0800

    邮箱设置

--- Framework.WpsWordDemo/Program.cs
+++ Framework.WpsWordDemo/Program.cs
@@ -12,20 +12,20 @@ namespace Framework.WpsWordDemo
     {
         static void Main(string[] args)
         {
-            ProcessControl processControl = new ConcreteProcessControl();
-            processControl.KillWordProcess();
-            string currDir = AppDomain.CurrentDomain.BaseDirectory;
-            string fileFullName = string.Format(@"{0}Demo\pdf导出.docx", currDir);
-            string outDirctoryUrl = string.Format(@"{0}NewFiles", currDir);
-            FileControl fileControl = new ConcreteFileControl();
-            fileControl.CreateDirectory(outDirctoryUrl);
-            string outFileFullName = string.Format(@"{0}NewFiles\{1}.pdf", currDir, Guid.NewGuid().ToString());
-            using (ToPdfHelper tph = new ToPdfHelper("docx")) {
-                tph.WordWpsToPdf(outFileFullName,fileFullName);
-            }
+            //ProcessControl processControl = new ConcreteProcessControl();
+            //processControl.KillWordProcess();
+            //string currDir = AppDomain.CurrentDomain.BaseDirectory;
+            //string fileFullName = string.Format(@"{0}Demo\pdf导出.docx", currDir);
+            //string outDirctoryUrl = string.Format(@"{0}NewFiles", currDir);
+            //FileControl fileControl = new ConcreteFileControl();
+            //fileControl.CreateDirectory(outDirctoryUrl);
+            //string outFileFullName = string.Format(@"{0}NewFiles\{1}.pdf", currDir, Guid.NewGuid().ToString());
+            //using (ToPdfHelper tph = new ToPdfHelper("docx")) {
+            //    tph.WordWpsToPdf(outFileFullName,fileFullName);
+            //}
 
-            Console.WriteLine("hellow world");
-            Console.ReadLine();
+            //Console.WriteLine("hellow world");
+            //Console.ReadLine();
         }
     }
 }
---
+++ MailKitDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ *nuget MailKit
+ */
+using System;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace MailKitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Joey Tribbiani", "334828328@qq.com"));
+            message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", "334828328@qq.com"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.qq.com", 25, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("334828328@qq.com", "zwsqtqnbefesbjdi");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+			Console.WriteLine("Hello World!");
+        }
+    }
+}
