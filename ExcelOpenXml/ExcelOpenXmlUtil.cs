ce4723dec82f9f24caa3e36578420ae514016036
sun198986 <334828328@qq.com>
2020-12-20 03:44:00 +0800

    Excel列合并

--- ExcelOpenXml/ExcelOpenXmlUtil.cs
+++ ExcelOpenXml/ExcelOpenXmlUtil.cs
@@ -4,6 +4,7 @@ using DocumentFormat.OpenXml.Spreadsheet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -39,20 +40,46 @@ namespace ExcelOpenXml
         }
 
 
-        //导入数据
-        public void ImportExcelDataForTemplate<T>(IEnumerable<T> list,uint rowIndex)
+        /// <summary>
+        /// 导入excel(需要模板,文件在外边复制)
+        /// </summary>
+        /// <typeparam name="T">导入对象</typeparam>
+        /// <param name="list">集合:需按照groupColumnProperty排序</param>
+        /// <param name="rowIndex">开始行写入</param>
+        /// <param name="columnInex">开始列写入</param>
+        /// <param name="groupColumnProperty">按照此属性合并单元格</param>
+        /// <param name="needMergeColumnProperty">需合并单元格属性名</param>
+        public void ImportExcelDataForTemplate<T>(IEnumerable<T> list, int rowIndex, int columnInex, string groupColumnProperty="", string needMergeColumnProperty="")
         {
+            if (list==null||list.Count()==0) {
+                Console.WriteLine($"集合不能为null");
+                throw new Exception($"集合不能为null");
+            }
             //打开可编辑的excel
             OpenExcel(true);
 
             Worksheet worksheet = GetWorksheet(_spreadsheetDocument, _sheetName);
 
             var sheetData = worksheet.GetFirstChild<SheetData>();
+
+            //上一个需要合并的列值
+            string foreValue = string.Empty;
+            //开始合并的行
+            int startMergeRow = rowIndex ;
+            //结束合并的行
+            int endMergeRow = rowIndex;
+            //需要合并的列
+            int needMergeColumn = 0;
+            //当前列的值
+            string currentValue = string.Empty;
+            //需要合并行的集合
+            List<MergeCellInfo> mergeCellInfos = new List<MergeCellInfo>();
+
             //遍历行
             for (var i = 0; i < list.Count(); i++)
             {
                 var item = list.ToList()[i];
-                uint cellRowIndex = rowIndex + (uint)i;
+                uint cellRowIndex = (uint)rowIndex + (uint)i;
                 Row row;
                 //行不存在
                 if (sheetData.Elements<Row>().Where(r => r.RowIndex == cellRowIndex).Count() == 0)
@@ -68,16 +95,52 @@ namespace ExcelOpenXml
 
                 //遍历插入数据 列上插入
                 var property = item.GetType().GetProperties();
+                
                 for (int j = 0; j < property.Length; j++)
                 {
                     var prop = property[j];
-                    var cellColumnIndex = j + 1;
+                    currentValue = prop.GetValue(item).ToString();
+                    var cellColumnIndex = columnInex + j + 1;
+                    //第一行查看需要合并列(默认第一行不需要合并)
+                    if (i == 0&&prop.Name== needMergeColumnProperty) {
+                        needMergeColumn = j+columnInex;
+                    }
+                    if (prop.Name == groupColumnProperty) {
+                        if (string.IsNullOrEmpty(foreValue))
+                            foreValue = currentValue;
+                        else if (currentValue != foreValue)
+                        {
+                            endMergeRow = int.Parse((cellRowIndex - 1).ToString());
+                            //需要合并的行startMergeRow到i
+                            if (startMergeRow != cellRowIndex - 1) {
+                                MergeCellInfo mergeCellInfo = new MergeCellInfo
+                                {
+                                    StartCell = GetCellName((uint)startMergeRow, needMergeColumn),
+                                    EndCell = GetCellName(cellRowIndex - 1, needMergeColumn)
+                                };
+                                mergeCellInfos.Add(mergeCellInfo);
+                            }
+                            startMergeRow = int.Parse(cellRowIndex.ToString());
+                            foreValue = currentValue;
+                        }
+                        //最后一行往前合并
+                        else if (i == list.Count() - 1) {
+                            if (currentValue == foreValue && cellRowIndex - 1 != endMergeRow) {
+                                MergeCellInfo mergeCellInfo = new MergeCellInfo
+                                {
+                                    StartCell = GetCellName((uint)startMergeRow, needMergeColumn),
+                                    EndCell = GetCellName(cellRowIndex, needMergeColumn)
+                                };
+                                mergeCellInfos.Add(mergeCellInfo);
+                            }
+                        }
+                    }
 
                     string reference = GetCellName(cellRowIndex, cellColumnIndex);
 
                     Cell dataCell = new Cell { CellReference = reference };
                     //如果行上的单元格已存在,修改单元格内容
-                    if (row.Elements<Cell>().Where(c => c.CellReference!=null && c.CellReference.Value == reference).Count() > 0)
+                    if (row.Elements<Cell>().Where(c => c.CellReference != null && c.CellReference.Value == reference).Count() > 0)
                     {
                         dataCell = row.Elements<Cell>().Where(s => s.CellReference == reference).FirstOrDefault();
                     }
@@ -104,13 +167,20 @@ namespace ExcelOpenXml
                         else
                             row.InsertBefore(dataCell, refCell);
                     }
-                    dataCell.CellValue = new CellValue(prop.GetValue(item).ToString());
+                    dataCell.CellValue = new CellValue(currentValue);
                     dataCell.DataType = new EnumValue<CellValues>(CellValues.String);
                 }
             }
 
             //遍历完之后合并单元格
-
+            Console.WriteLine(mergeCellInfos.Count);
+            if (mergeCellInfos.Count > 0) {
+                foreach (var item in mergeCellInfos)
+                {
+                    this.MergeCells(item.StartCell, item.EndCell);
+                }
+            }
+           
             _spreadsheetDocument.Save();
         }
 
@@ -168,10 +238,9 @@ namespace ExcelOpenXml
         }
 
 
-        public void MergeCells(string fileName, string sheetName, string startCell, string endCell)
+        private void MergeCells(string startCell, string endCell)
         {
-            OpenExcel(true);
-            Worksheet worksheet = GetWorksheet(_spreadsheetDocument, sheetName);
+            Worksheet worksheet = GetWorksheet(_spreadsheetDocument, _sheetName);
             // Create Spreadsheet cells.  
             CreateSpreadsheetCell(worksheet, startCell);
             CreateSpreadsheetCell(worksheet, endCell);
@@ -197,7 +266,6 @@ namespace ExcelOpenXml
                 new StringValue(startCell + ":" + endCell)
             };
             mergeCells.Append(mergeCell);
-            worksheet.Save();
         }
 
         private string GetCellName(uint rowIndex, int columnIndex)
@@ -209,7 +277,8 @@ namespace ExcelOpenXml
         private void CreateSpreadsheetCell(Worksheet worksheet, string cellName)
         {
             string columnName = GetColumnName(cellName);
-            uint rowIndex = 2;
+            string strSplit1 = Regex.Replace(cellName, "[0-9]", "", RegexOptions.IgnoreCase);
+            int rowIndex = ExcelAlphabet.ABCToColumn(strSplit1);
             IEnumerable<Row> rows = worksheet.Descendants<Row>().Where(r => r
                 .RowIndex.Value == rowIndex);
             Row row = rows.First();
@@ -233,5 +302,13 @@ namespace ExcelOpenXml
             GC.SuppressFinalize(this);
             //throw new NotImplementedException();
         }
+    }
+
+    class MergeCellInfo
+    {
+
+        public string StartCell { get; set; }
+
+        public string EndCell { get; set; }
     }
 }
--- ExcelOpenXml/Program.cs
+++ ExcelOpenXml/Program.cs
@@ -7,18 +7,18 @@ namespace ExcelOpenXml
     {
         static void Main(string[] args)
         {
-            string fileFullName = @"E:\Common_Soft\Template.xlsx";
+            string fileFullName = @"D:\Document\Template.xlsx";
             string sheetName = "Sheet1";
             IEnumerable<ExcelDataModel> iel = new List<ExcelDataModel>
             {
+                new ExcelDataModel("a1","b2","c3","d4","e5","f6","g7","h8","i9","j10"),
+                new ExcelDataModel("a1","b2","c3","d4","e5","f6","g7","h8","i9","j10"),
+                new ExcelDataModel("a1","b2","c3","d4","e5","f6","g7","h8","i9","j10"),
                 new ExcelDataModel("a2","b2","c3","d4","e5","f6","g7","h8","i9","j10"),
-                new ExcelDataModel("a1","b2","c3","d4","e5","f6","g7","h8","i9","j10"),
-                new ExcelDataModel("a1","b2","c3","d4","e5","f6","g7","h8","i9","j10"),
-                new ExcelDataModel("a1","b2","c3","d4","e5","f6","g7","h8","i9","j10"),
                 new ExcelDataModel("a1","b2","c3","d4","e5","f6","g7","h8","i9","j10")
             };
             using (ExcelOpenXmlUtil excelOpenXmlUtil = new ExcelOpenXmlUtil(fileFullName, sheetName)) {
-                excelOpenXmlUtil.ImportExcelDataForTemplate(iel,10);
+                excelOpenXmlUtil.ImportExcelDataForTemplate(iel,10,2,"AA","J");
             }
             Console.WriteLine("Hello World!");
             Console.ReadLine();
