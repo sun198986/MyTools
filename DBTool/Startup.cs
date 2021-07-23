ac5a33b1f6e3f9ca2ca0a8067e8f0efd0a2c23d3
sun198986 <334828328@qq.com>
2021-07-24 00:05:56 +0800

    修改样式

--- DBTool/Startup.cs
+++ DBTool/Startup.cs
@@ -26,7 +26,7 @@ namespace DBTool
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IDBContext>((p)=> {
-               return new DapperDBContext(Configuration.GetConnectionString("MASS"));
+               return new DapperDBContext(Configuration.GetConnectionString("Test"));
             });
             services.AddControllersWithViews();
             services.AddScoped<StringHelper>();
