9c0abe1884daddb5b63045d4424e3608569189da
孙士杰 <334828328@qq.com>
2021-08-05 23:55:25 +0800

    123

--- ThreadDemo/Demo.cs
+++ ThreadDemo/Demo.cs
@@ -11,8 +11,36 @@ namespace ThreadDemo
 
         public List<string> GetList()
         {
+            Func<string,string> t = DoSomethingResult;
+            var s= t("123");
+
+            DisplayValue(s1=>s1+"333", "222");
+
+            Task.Run(() =>
+            {
+                Console.WriteLine("123");
+            });
+            
             Parallel.Invoke(DoSomething,DoSomething2,DoSomething3,DoSomething4);
             return ListDemo;
+        }
+
+        private string DisplayValue(Func<string,string> strFunc,string para)
+        {
+            string v = strFunc(para);
+            return v;
+        }
+
+        private string DisplayValue1(Action<string, string> strAction, string para)
+        {
+            strAction(para,para+1);
+            return para;
+        }
+
+
+        private string DoSomethingResult(string hello)
+        {
+            return $"返回值";
         }
 
         private void DoSomething()
--- ThreadDemo/Program.cs
+++ ThreadDemo/Program.cs
@@ -12,55 +12,55 @@ namespace ThreadDemo
 
         static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                List<Task<List<string>>> tasks = new List<Task<List<string>>>();
-                //开始时间
-                DateTime dtF = DateTime.Now;
+                Demo demo = new Demo();
+                demo.GetList();
 
-                //事务1
-                var task = Task<List<string>>.Factory.StartNew(() =>
-                {
-                    var list = new List<string>();
-                    Thread.Sleep(2000);
-                    list.Add(Guid.NewGuid().ToString());
-                    return list;
-                });
-                tasks.Add(task);
+                //    List<Task<List<string>>> tasks = new List<Task<List<string>>>();
+                //    //开始时间
+                //    DateTime dtF = DateTime.Now;
 
-                //事务2
-                var task1 = Task<List<string>>.Factory.StartNew(() =>
-                {
-                    var list = new List<string>();
-                    Thread.Sleep(2000);
-                    list.Add(Guid.NewGuid().ToString());
-                    return list;
-                });
-                tasks.Add(task1);
+                //    //事务1
+                //    var task = Task<List<string>>.Factory.StartNew(() =>
+                //    {
+                //        var list = new List<string>();
+                //        Thread.Sleep(2000);
+                //        list.Add(Guid.NewGuid().ToString());
+                //        return list;
+                //    });
+                //    tasks.Add(task);
 
-                //事务3
-                var task2 = Task<List<string>>.Factory.StartNew(() =>
-                {
-                    var list = new List<string>();
-                    Thread.Sleep(2000);
-                    list.Add(Guid.NewGuid().ToString());
-                    return list;
-                });
-                tasks.Add(task2);
+                //    //事务2
+                //    var task1 = Task<List<string>>.Factory.StartNew(() =>
+                //    {
+                //        var list = new List<string>();
+                //        Thread.Sleep(2000);
+                //        list.Add(Guid.NewGuid().ToString());
+                //        return list;
+                //    });
+                //    tasks.Add(task1);
 
-                //获取三个事务的返回值
-                List<string> res = new List<string>();
-                foreach (var item in tasks)
-                {
-                    res.AddRange(item.Result);
-                }
+                //    //事务3
+                //    var task2 = Task<List<string>>.Factory.StartNew(() =>
+                //    {
+                //        var list = new List<string>();
+                //        Thread.Sleep(2000);
+                //        list.Add(Guid.NewGuid().ToString());
+                //        return list;
+                //    });
+                //    tasks.Add(task2);
 
-                //结束时间
-                DateTime dtT = DateTime.Now;
-                //计算总用时
-                var dtSp = dtT - dtF;
-                Console.WriteLine($"结果：{string.Join(",", res)}   共计用时:{dtSp.TotalSeconds}秒");
-            }
+                //    //获取三个事务的返回值
+                //    List<string> res = new List<string>();
+                //    foreach (var item in tasks)
+                //    {
+                //        res.AddRange(item.Result);
+                //    }
+
+                //    //结束时间
+                //    DateTime dtT = DateTime.Now;
+                //    //计算总用时
+                //    var dtSp = dtT - dtF;
+                //    Console.WriteLine($"结果：{string.Join(",", res)}   共计用时:{dtSp.TotalSeconds}秒");
 
         }
 
